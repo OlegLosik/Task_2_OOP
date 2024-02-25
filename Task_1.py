914f7d5eb77761a36591d8ef2cbf24e957617857
Losik <oleg20.96@mail.ru>
2024-02-25 20:06:59 +0300

    Change Task_1_2

--- Task_1.py
+++ Task_1.py
@@ -1,17 +1,21 @@
-import os
-def food_list():
-    with open ('Receipes.txt', 'r', encoding='utf8') as receipes:
+def cook_book():
+    with open("Receipes.txt", encoding="utf-8") as file:
         cook_book = {}
-        for line in receipes:
-            food_name = line.strip()
-            cook_book [food_name] = []
-            products_count = receipes.readline()
-            for _ in range(int(products_count)):
-                prod = receipes.readline()
-                ingredient_name, quantity, measure = prod.split('|')
-                receipe = {'ingredient_name':ingredient_name, 'quantity':quantity, 'measure':measure.split()}
-                cook_book[food_name].append(receipe)
-            receipes.readline()
-            print(cook_book)
-    return
-food_list()
+        for line in file.read().split("\n\n"):
+            name, _, *products = line.split("\n")
+            cook_list = []
+            for prod in products:
+                ingredient_name, quantity, measure = map(
+                    lambda x: int(x) if x.isdigit() else x, prod.split(" | ")
+                )
+                cook_list.append(
+                    {
+                        "ingredient_name": ingredient_name,
+                        "quantity": quantity,
+                        "measure": measure,
+                    }
+                )
+            cook_book[name] = cook_list
+    return cook_book
+
+print(cook_book())
--- Task_2.py
+++ Task_2.py
@@ -1,32 +1,42 @@
-import os
-def food_list():
-    with open ('Receipes.txt', 'r', encoding='utf8') as receipes:
+def cook_book():
+    with open("Receipes.txt", encoding="utf-8") as file:
         cook_book = {}
-        for line in receipes:
-            food_name = line.strip()
-            cook_book [food_name] = []
-            products_count = receipes.readline()
-            for _ in range(int(products_count)):
-                prod = receipes.readline()
-                ingredient_name, quantity, measure = prod.split('|')
-                receipe = {'ingredient_name':ingredient_name, 'quantity':quantity, 'measure':measure.split()}
-                cook_book[food_name].append(receipe)
-            receipes.readlines()
-        #task_2
-        person_count = 2
-        shop_list = {}
-        for _ in cook_book:
-            total = cook_book[food_name]
-            for costs in total:
-                shop_list_by_dish = costs
-                name_product = shop_list_by_dish['ingredient_name']
-                weight = shop_list_by_dish['measure']
-                amount = int (shop_list_by_dish['quantity']) * person_count
-                weight_amount = {'measure' : weight, 'quantity' : amount}
-                if name_product not in shop_list:
-                    shop_list[name_product] = weight_amount
+        for line in file.read().split("\n\n"):
+            name, _, *products = line.split("\n")
+            cook_list = []
+            for prod in products:
+                ingredient_name, quantity, measure = map(
+                    lambda x: int(x) if x.isdigit() else x, prod.split(" | ")
+                )
+                cook_list.append(
+                    {
+                        "ingredient_name": ingredient_name,
+                        "quantity": quantity,
+                        "measure": measure,
+                    }
+                )
+            cook_book[name] = cook_list
+    return cook_book
+
+#Task_2
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    cook_book_V = cook_book()
+    for dish in dishes:
+        if dish in cook_book_V:
+            for ingredient in cook_book_V[dish]:
+                ingredient_name = ingredient["ingredient_name"]
+                quantity = ingredient["quantity"] * person_count
+                measure = ingredient["measure"]
+                if ingredient_name not in shop_list:
+                    shop_list[ingredient_name] = {
+                        "quantity": quantity,
+                        "measure": measure,
+                    }
                 else:
-                    shop_list[name_product]['quantity'] = shop_list[name_product]['quantity'] + amount
-        print (shop_list)
-    return
-food_list()
+                    shop_list[ingredient_name]["quantity"] += quantity
+    return shop_list
+
+
+result = get_shop_list_by_dishes(["Запеченный картофель", "Омлет"], 2)
+print(result)
